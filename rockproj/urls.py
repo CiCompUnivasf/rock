548b011910ab79369d18bc352d90de9b857eeff4
Raul <raulrufino0@gmail.com>
2023-05-01 13:55:31 -0300

    Altera o nome do django project `rocksite` para `rockproj`

--- rockproj/urls.py
+++ rockproj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for rocksite project.
+URL configuration for rockproj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- rockproj/wsgi.py
+++ rockproj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rocksite project.
+WSGI config for rockproj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rocksite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rockproj.settings')
 
 application = get_wsgi_application()
