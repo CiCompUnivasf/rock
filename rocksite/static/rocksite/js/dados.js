e3941a85080ccb7c9cc71a43b029eb3c374fd933
gabriel ferreira <gabrielgabrielrodrigues25@hotmail.com>
2023-05-23 09:43:44 -0300

    Visualização da tabela de resultados

--- rocksite/static/rocksite/js/dados.js
+++ rocksite/static/rocksite/js/dados.js
@@ -7,7 +7,7 @@ function submitForm(event) {
 
   for (let i = 0; i < fieldsCounter; i++) {
     const propriedade = document.getElementById(`propriedade${i}`).value;
-    const porcentagem = parseFloat(document.getElementById(`porcentagem${i}`).value.replaceAll(",","."));
+    const porcentagem = parseFloat(document.getElementById(`porcentagem${i}`).value.replaceAll(",", "."));
     if (propriedade && porcentagem) {
       propriedades.push(propriedade);
       porcentagens.push(porcentagem);
@@ -19,22 +19,49 @@ function submitForm(event) {
     searchParams.append('propriedade[]', propriedades[i]);
     searchParams.append('porcentagem[]', porcentagens[i]);
   }
-  const payload = {propriedades : propriedades , porcentagens : porcentagens}
+  const payload = { propriedades: propriedades, porcentagens: porcentagens }
   console.log(payload)
   console.log(`Será buscado as seguintes propriedades: ${propriedades}`)
   console.log(`Será buscado os seguintes valores: ${porcentagens}`)
   // TODO: Corrigir esse código para realizar requisição POST com propiedades e porcentagens sendo enviadas via JSON
   const url = '/search';
-  const option = {method: 'POST', body: JSON.stringify(payload)}
+  const option = { method: 'POST', body: JSON.stringify(payload) }
   fetch(url, option)
-  .then(response => response.json())
-  .then(data => {
-    console.log (data)
-  //    // Manipular os dados retornados e preencher a tabela
-  //    // de acordo com os resultados da busca
-  })
- .catch(error => {
-  console.error('Ocorreu um erro na busca:', error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      //    // Manipular os dados retornados e preencher a tabela
+      //    // de acordo com os resultados da busca
+      // Obtém a referência ao corpo da tabela
+      var tbody = document.getElementById("tables_localizacao");
+
+      // Percorre o JSON e cria as linhas da tabela
+      data.forEach(function (obj) {
+        // Cria uma nova linha na tabela
+        let row = document.createElement("tr");
+
+        let descricaoCell = document.createElement("td");
+        descricaoCell.textContent = obj.descricao;
+
+        // Cria as células para o município e a UF
+        let municipioCell = document.createElement("td");
+        municipioCell.textContent = obj.municipio;
+
+        let ufCell = document.createElement("td");
+        ufCell.textContent = obj.uf;
+
+        // Adiciona as células à linha
+        row.appendChild(descricaoCell);
+        row.appendChild(municipioCell);
+        row.appendChild(ufCell);
+
+        // Adiciona a linha ao corpo da tabela
+        tbody.appendChild(row);
+      });
+
+    })
+    .catch(error => {
+      console.error('Ocorreu um erro na busca:', error);
+    });
 }
 
