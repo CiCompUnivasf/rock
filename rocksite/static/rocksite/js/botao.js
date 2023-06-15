4d0c6f0210476679d0877348e3026ff06345331a
Raul <raulrufino0@gmail.com>
2023-06-15 09:09:44 -0300

    Adiciona select no formulário

--- rocksite/static/rocksite/js/botao.js
+++ rocksite/static/rocksite/js/botao.js
@@ -8,7 +8,11 @@ const form = document.querySelector('form');
 function addFields() {
   const div = document.createElement('div');
   div.innerHTML = `
-    <input type="text" id="propriedade${fieldsCounter}" placeholder="Propriedade">
+    <select id="propriedade${fieldsCounter}">
+      <option value="h2o"> H2O (pH) </option>
+      <option value="calcio">Cálcio (cmol/kg) </option>
+      <option value="kcl">Cloreto de Potássio (pH) </option>
+    </select>
     <input type="text" id="porcentagem${fieldsCounter}" placeholder="Porcentagem">
   `;
   form.insertBefore(div, addButton);
